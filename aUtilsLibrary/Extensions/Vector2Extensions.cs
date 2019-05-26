0b3d518d8c779b21b70c5743f72fb386e4e95fa2
Guendolin <floggingluna@gmail.com>
2019-05-26 16:09:00 +0200

    Added two basic Vector2 extension functions.

---
+++ aUtilsLibrary/Extensions/Vector2Extensions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public static class Vector2Extensions
+{
+
+    /// <summary>
+    /// Returns true  if values is within the x (inclusive) and y (exlusive) otherwise false
+    /// </summary>
+    public static bool InRange(this Vector2 v, float value)
+    {
+        return value >= v.x && value < v.y;
+    }
+
+    /// <summary>
+    /// Return a random float number between x [inclusive] and y [inclusive]
+    /// </summary>
+    public static float Random(this Vector2 v)
+    {
+        return UnityEngine.Random.Range(v.x, v.y);
+    }
+}
