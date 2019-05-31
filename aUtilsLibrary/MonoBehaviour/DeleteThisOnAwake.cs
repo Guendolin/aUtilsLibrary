0011c2539c5429d1a8a6b4cca4aafe70a5350528
Guendolin <floggingluna@gmail.com>
2019-05-31 16:45:41 +0200

    Added Delete monobehaviors so that you can have test objects in the scene that is destroyed when you start playing.

---
+++ aUtilsLibrary/MonoBehaviour/DeleteThisOnAwake.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace aSystem.aUtilsLibrary
+{
+    public class DeleteThisOnAwake : MonoBehaviour
+    {
+        public void Awake()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
+
