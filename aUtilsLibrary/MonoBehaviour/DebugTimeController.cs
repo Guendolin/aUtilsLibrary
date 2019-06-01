cc3b72eb72a748ffab348a45f6641d943d17e2c9
Guendolin <floggingluna@gmail.com>
2019-06-01 12:20:54 +0200

    Added MonoBehaviour to slow down time for debug purposes.

---
+++ aUtilsLibrary/MonoBehaviour/DebugTimeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugTimeController : MonoBehaviour
+{
+    #if UNITY_EDITOR
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Time.timeScale = Time.timeScale < 1 ? 1f : 0.2f;
+        }
+    }
+    #endif
+}
